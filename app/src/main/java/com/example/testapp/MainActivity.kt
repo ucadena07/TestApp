e50389a2ecb53a5288ad108b9de0889db59eb3fb
Ulises Cadena <ucadena07@gmail.com>
2024-02-07 06:53:07 -0600

    di setup

--- app/src/main/java/com/example/testapp/MainActivity.kt
+++ app/src/main/java/com/example/testapp/MainActivity.kt
@@ -15,7 +15,9 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.testapp.network.ApplicationNavigation
 import com.example.testapp.ui.theme.TestAppTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
