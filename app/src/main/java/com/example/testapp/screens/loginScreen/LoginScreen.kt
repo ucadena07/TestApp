f6f1a9210e804b8c2823e134190d74f4693f29fc
Ulises Cadena <ucadena07@gmail.com>
2024-02-08 06:43:55 -0600

    normal text component

--- app/src/main/java/com/example/testapp/screens/loginScreen/LoginScreen.kt
+++ app/src/main/java/com/example/testapp/screens/loginScreen/LoginScreen.kt
@@ -1,19 +1,36 @@
 package com.example.testapp.screens.loginScreen
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
+import com.example.testapp.components.shared.NormalTextComponent
 import com.example.testapp.network.ApplicationScreens
 
 @Composable
-fun LoginScreen(navController: NavHostController) {
-    Column {
-        Text(text = "Login Screen")
-        Button(onClick = { navController.navigate(ApplicationScreens.HomeScreen.name) }) {
-            Text(text = "Navigate Home")
-        }
-    }
+fun LoginScreen(navController: NavHostController?) {
+  Surface(
+      color = Color.White,
+      modifier = Modifier
+          .fillMaxSize()
+          .background(Color.White)
+          .padding(28.dp)
+  ) {
+      NormalTextComponent(value = "Welcome")
+  }
 
+}
+@Preview
+@Composable
+fun DefaultPreviewForLoginScreen(){
+    LoginScreen(navController = null)
 }
