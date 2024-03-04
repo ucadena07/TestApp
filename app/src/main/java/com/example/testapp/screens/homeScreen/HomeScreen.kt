39d9cb0bd5af8f49cb5b9beee0e3cf19f096db2a
Ulises Cadena <ucadena07@gmail.com>
2024-03-04 06:41:42 -0600

    add date format utils

--- app/src/main/java/com/example/testapp/screens/homeScreen/HomeScreen.kt
+++ app/src/main/java/com/example/testapp/screens/homeScreen/HomeScreen.kt
@@ -1,11 +1,20 @@
 package com.example.testapp.screens.homeScreen
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import com.example.testapp.authentication.AuthState
+import com.example.testapp.screens.loginScreen.formatDate
+import com.example.testapp.screens.loginScreen.formatDateTime
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
-    Text(text = "Welcome ${AuthState.getAuthDetails()?.name}")
+    Column {
+        Text(text = "Welcome ${AuthState.getAuthDetails()?.name}")
+        Text(text = "${AuthState.getAuthDetails()?.emailAddress}")
+        Text(text = "Token exp: ${formatDate(AuthState.getAuthDetails()?.exp!!)} @ ${formatDateTime(AuthState.getAuthDetails()?.exp!!)} ")
+    }
+
+
 }
