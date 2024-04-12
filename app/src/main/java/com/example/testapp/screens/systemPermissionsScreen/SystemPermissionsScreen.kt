192da2ab8ca5e17c83f7703c56a55d4eb9def7bf
Ulises Cadena <ucadena07@gmail.com>
2024-04-12 07:01:39 -0500

    more permissions

--- app/src/main/java/com/example/testapp/screens/systemPermissionsScreen/SystemPermissionsScreen.kt
+++ app/src/main/java/com/example/testapp/screens/systemPermissionsScreen/SystemPermissionsScreen.kt
@@ -1,10 +1,44 @@
 package com.example.testapp.screens.systemPermissionsScreen
 
+import android.Manifest
+import androidx.activity.compose.rememberLauncherForActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
+import com.example.testapp.layout.MainLayout
 
 @Composable
 fun SystemPermissionsScreen(navController: NavHostController, vm: SystemPermissionScreenVm) {
-    Text(text = "Permissions Screen")
+    MainLayout(navController = navController) {
+
+        val dialogQueue = vm.visiblePermissionDialogQueue
+        val cameraPermissionResultLauncher = rememberLauncherForActivityResult(
+            contract = ActivityResultContracts.RequestPermission(),
+            onResult = {isGranted ->
+                vm.onPermissionResult(
+                    permission = Manifest.permission.CAMERA,
+                    isGranted = isGranted
+                )
+            }
+        )
+        Column (
+            modifier = Modifier.fillMaxWidth(),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ){
+            Button(onClick = { cameraPermissionResultLauncher.launch(Manifest.permission.CAMERA) }) {
+                Text(text = "Request one permission")
+            }
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Request multiple permission")
+            }
+        }
+    }
 }
